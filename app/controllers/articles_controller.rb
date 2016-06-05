4e8c10ae0b7e6ba798943e600c4e59f54854d2a9
Stephen Mayeux <stephenmayeux@gmail.com>
2016-06-05 21:31:13 +0900

    create show route. add CSS. update view and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
-  # we need to use instance variables to make it available to the view 
+
+  # we need to use instance variables to make it available to the view
   def index
-    @articles = Article.new
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
 end
